66b35d05cd10ceeca30fbfcfeac2da7da582ae82
assabbour <assabbour@hotmail.com>
2024-12-07 14:18:56 +0100

    Ajeut des tests unitaires pour la methode GET

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'mabanque' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mabanque');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mabanque');
+  })
+
+  it('dervait afficher le titre<h1>',()=>{
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenue sur Ma Banque 💳')
   });
+
+  it('devrait contenir un tableau avec trois colonnes',()=>{
+    const compiled = fixture.nativeElement;
+    const headers = compiled.querySelectorAll('th');
+    expect(headers.length).toBe(3);
+    expect(headers[0].textContent).toContain('Nom de la transition');
+    expect(headers[1].textContent).toContain('Montant');
+    expect(headers[2].textContent).toContain('Date');
+  })
+
 });
--- src/app/transactions.service.spec.ts
+++ src/app/transactions.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TransactionsService } from './transactions.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { transition } from '@angular/animations';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let httpMock: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        imports:[HttpClientTestingModule],
+        providers:[TransactionsService]
+    });
     service = TestBed.inject(TransactionsService);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+ 
+  afterEach(()=>{
+    httpMock.verify();
+  })
+
+
+  it('devrait faire une requete GET et retourner des données', ()=>{
+    const mockTransactions = [
+        {name:"Paiement", amount:-50, date:"2024-12-01"},
+        {name : "Salaire", amount:1500, date:"2024-12-05"}
+    ]
+
+    service.getTransactins().subscribe((transactions)=>{
+        expect(transactions.length).toBe(2);
+        expect(transactions).toEqual(mockTransactions);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/transactions');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockTransactions);
+
+  })
+
+
 });
